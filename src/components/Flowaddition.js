fbac9e40f4d671434a0d788e36509e9b61e52538
Agarwal <p.m.agarwal@accenture.com>
2022-02-14 17:26:09 +0530

    click issue

--- src/components/Flowaddition.js
+++ src/components/Flowaddition.js
@@ -1,37 +1,39 @@
-import React,{useState,useCallback, useEffect} from 'react'
+import React, { useState, useCallback, useEffect } from 'react'
 import "../styles/FlowStyles.css"
-import ReactFlow,{
+import ReactFlow, {
   addEdge,
+  ReactFlowProvider
 } from 'react-flow-renderer';
 
-export const Flowaddition=()=> {
-  
+export const Flowaddition = () => {
 
-  const initialElements  = [
+
+  const initialElements = [
     {
       id: '1',
       type: 'input', // input node
-      data: 
-       { label: <select>
-         <option value='red'>Red</option>
-         <option value='purple'>Purple </option>
-       </select> },
+      data:
+      {
+        label: <select>
+          <option value='red'>Red</option>
+          <option value='purple'>Purple </option>
+        </select>
+      },
       position: { x: 245, y: 36 },
-      style:{
+      style: {
         backgroundColor: "whitesmoke",
         borderRadius: "25px",
         height: "77px"
-    },
-}
-]
+      },
+    }
+  ]
 
-const getNodeId = () => `randomnode_${+new Date()}`;
+  const getNodeId = () => `randomnode_${+new Date()}`;
 
-const [elements, setElements] = useState(initialElements);
-const onConnect = (params) => setElements((els) => addEdge(params, els));
-  
-const onAdd = useCallback(() => {
-    console.log("Hi");
+  const [elements, setElements] = useState(initialElements);
+  const onConnect = (params) => setElements((els) => addEdge(params, els));
+
+  const onAdd = useCallback(() => {
     const newNode = {
       id: getNodeId(),
       data: { label: 'Added node' },
@@ -39,22 +41,25 @@ const onAdd = useCallback(() => {
         x: Math.random() * window.innerWidth - 100,
         y: Math.random() * window.innerHeight,
       },
-      style:{
+      style: {
         backgroundColor: "whitesmoke",
         borderRadius: "25px",
         height: "77px"
-    },
+      },
     };
     setElements((els) => els.concat(newNode));
   }, [setElements]);
-  
-  
+
+
   return (
-    <div className='flowaddition'> 
-        <div style={{width:'100%',height:'100vh'}}>
-        <ReactFlow elements={elements} onConnect={onConnect}/>
-        <button className='button' onClick={onAdd}>+</button>
-        </div>
+    <div className='flowaddition'>
+      <div style={{ width: '100%', height: '100vh' }}>
+        {/* <ReactFlowProvider> */}
+          <ReactFlow elements={elements} onConnect={onConnect}>
+            <button className='button' onClick={onAdd}>+</button>
+          </ReactFlow>
+        {/* </ReactFlowProvider> */}
+      </div>
     </div>
   )
 }
