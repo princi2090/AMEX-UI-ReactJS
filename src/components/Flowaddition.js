36f9f7d806b7d6441770ce45999653efcd5d56e6
Sahu <siddharth.b.sahu@accenture.com>
2022-02-15 21:15:55 +0530

    Icons added for apac flow

--- src/components/Flowaddition.js
+++ src/components/Flowaddition.js
@@ -6,17 +6,22 @@ import ReactFlow, {
 } from 'react-flow-renderer';
 import CalculateIcon from '@mui/icons-material/Calculate';
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import {useNavigate} from "react-router-dom";
 
 export const Flowaddition = () => {
 
-
+  const navigate=useNavigate();
   const initialElements = [
     {
       id: '1',
       type: 'input', // input node
       data:
       {
-        label: <CalculateIcon/>
+        label:<div>
+        <CalculateIcon/>
+        <br/>
+        Apac Monthly Flow
+        </div>
         
         // <select>
         //   <option value='red'>Red</option>
@@ -24,6 +29,29 @@ export const Flowaddition = () => {
         // </select>
       },
       position: { x: 245, y: 36 },
+      style: {
+        backgroundColor: "whitesmoke",
+        borderRadius: "25px",
+        height: "77px"
+      },
+    },
+    {
+      id: 2,
+      data: { 
+        label: <div onClick={()=>navigate("/monthlyFlow")}>
+        <CalendarTodayIcon/>
+        <br/>
+        Recipe 
+        </div>
+      },
+      type:'default',
+      position: {
+        // x: Math.random() * window.innerWidth - 100,
+        // y: Math.random() * window.innerHeight,
+        //472.76,37.1204
+        x: 472.76, 
+        y: 37.1204
+      },
       style: {
         backgroundColor: "whitesmoke",
         borderRadius: "25px",
@@ -43,7 +71,7 @@ export const Flowaddition = () => {
     },
 
     { id: 'e1-2', source: '1', target: '2', animated: true },
-    { id: 'e1-23', source: '1', target: '4', animated: true },
+    { id: 'e1-23', source: '2', target: '3', animated: true },
   ]
 
   const getNodeId = () => `randomnode_${+new Date()}`;
@@ -54,7 +82,9 @@ export const Flowaddition = () => {
   const onAdd = useCallback(() => {
     const newNode = {
       id: getNodeId(),
-      data: { label: <CalendarTodayIcon/> },
+      data: { 
+        label: <CalendarTodayIcon/> 
+      },
       type:'default',
       position: {
         // x: Math.random() * window.innerWidth - 100,
@@ -78,7 +108,8 @@ export const Flowaddition = () => {
       <div style={{ width: '100%', height: '100vh' }}>
         {/* <ReactFlowProvider> */}
           <ReactFlow elements={elements} onConnect={onConnect}>
-            <button className='button' onClick={onAdd}>
+            {/* <button className='button' onClick={onAdd}> */}
+            <button className='button'>
             +
             </button>
           </ReactFlow>
